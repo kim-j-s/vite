fcff39c18c9e2e99518c11a04fe8a3918b46ab0e
kogwing <kogwing@gmail.com>
2024-04-02 10:14:31 +0900

    .prettieric 설정 중

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { resolve } from "path";
-// import path from "path";
-import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
-import handlebars from "vite-plugin-handlebars";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
 
-const root = resolve(__dirname, "src");
-const outDir = resolve(__dirname, "dist");
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
 
 export default defineConfig({
   // base: "/",
@@ -13,25 +11,32 @@ export default defineConfig({
   build: {
     // 배포 때 풀더 지우기
     emptyOutDir: true,
-    outDir: "../dist",
-    // assetsInlineLimit: 0, // 모든 파일을 번들링하도록 설정
-    // rollupOptions: {
-    //   input: {
-    //     main: resolve(__dirname, "index.html"),
-    //   },
-    // },
+    outDir: '../dist',
   },
   plugins: [
     handlebars({
       // handlebar 경로 설정
       // partialDirectory: path.resolve(__dirname, "src/_partials"),
-      partialDirectory: resolve(__dirname, "_partials"),
+      partialDirectory: resolve(__dirname, '_partials'),
       // public 폴더를 처리할 수 있도록 rootDir 설정
       // rootDir: resolve(__dirname, "public"),
     }),
-    // createHtmlPlugin({
+    // html({
     //   minify: true,
-    //   pagesDir: "src",
+    //   // inject: {
+    //   //   injectData: {
+    //   //     title: 'My App',
+    //   //   },
+    //   // },
+    //   // src/pages 디렉토리 내의 모든 HTML 파일을 가져와서 페이지로 처리합니다.
+    //   pagesDir: 'src/pages',
+    // }),
+    // 단순 복사 배포
+    // copy({
+    //   targets: [
+    //     // src/assets/css/lib 디렉토리 내의 파일을 dist/assets/css/lib 디렉토리로 복사합니다.
+    //     { src: 'src/assets/css/lib/**/*', dest: 'dist/assets/css/lib' },
+    //   ],
     // }),
   ],
 });
